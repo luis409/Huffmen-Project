7bf0ed6f393984654040b18f2ccd2f24a327b70a
luis409 <luisgabrielalencar669@gmail.com>
2026-07-10 06:50:08 -0300

    frequency table ADT defined

--- main.c
+++ main.c
@@ -4,6 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAM 256
+
+unsigned char *alloc_frequency_table();
+void initialize_frequency_table(unsigned char *frequency_table);
+void fill_frequency_table(unsigned char *frequency_table, const char *file_name);
+void display_frequency_table();
+void destroy_frequency_table();
+
+
 int main()
 {
     return 0;
